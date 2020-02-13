9af0b7d3da3d9d996a97e8c1c17c53be4581374b
k-romero <imkevinromero@gmail.com>
2020-02-13 15:47:57 -0500

    made loops to check rows and diagonals for winning combination for specific piece

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,7 +4,90 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
-    public Board(Character[][] matrix) {
+
+    private static Character[][] = board;
+    public Board(Character[][] inputBoard) {
+        this.board = inputBoard;
+    }
+
+    public boolean rowsCheck(Character playerPiece){
+        boolean outcome = false;
+        int boardSize = 3;
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                //innerLoop checks all positions
+                if(board[i][j] != playerPiece){
+                    outcome = false;
+                    break;
+                } else { outcome = true;
+                    break;
+                }
+            }
+            if (outcome){
+                break;
+            }
+        }
+        return outcome;
+    }
+
+    public boolean colsCheck(Character playerPiece){
+        boolean outcome = false;
+        int boardSize = 3;
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                //innerLoop checks all positions
+                if(board[j][i] != playerPiece){
+                    outcome = false;
+                    break;
+                } else { outcome = true;
+                    break;
+                }
+            }
+            if (outcome){
+                break;
+            }
+        }
+        return outcome;
+    }
+
+    public boolean topLeftBotRightCheck(Character playerPiece){
+        boolean outcome = false;
+        int boardSize = 3;
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                //innerLoop checks all positions
+                if(board[i][i] != playerPiece){
+                    outcome = false;
+                    break;
+                } else { outcome = true;
+                    break;
+                }
+            }
+            if (outcome){
+                break;
+            }
+        }
+        return outcome;
+    }
+
+    public boolean topRightBotLeftCheck(Character playerPiece){
+        boolean outcome = false;
+        int boardSize = 3;
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                //innerLoop checks all positions
+                if(board[i][i] != playerPiece){
+                    outcome = false;
+                    break;
+                } else { outcome = true;
+                    break;
+                }
+            }
+            if (outcome){
+                break;
+            }
+        }
+        return outcome;
     }
 
     public Boolean isInFavorOfX() {
